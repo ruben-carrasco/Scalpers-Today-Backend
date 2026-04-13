f5e04af9ee47a52304cf77fb7c1b37511cb9a141
Ruben <rubencarrascofrias@gmail.com>
2026-04-13 12:26:18 +0200

    refactor(infrastructure): remove obsolete database connection wrapper

--- src/scalper_today/infrastructure/database/connection.py
+++
@@ -1,5 +0,0 @@
-"""Backward-compatible alias for database manager module."""
-
-from .database_manager import DatabaseManager, get_db_session, get_db_url
-
-__all__ = ["DatabaseManager", "get_db_session", "get_db_url"]
