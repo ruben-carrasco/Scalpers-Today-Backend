17d156a431b4e9a9da7224984ce27b10c1afe744
Ruben <rubencarrascofrias@gmail.com>
2026-04-13 12:27:29 +0200

    refactor(api): remove obsolete routes package aliases

--- src/scalper_today/api/routes/__init__.py
+++ src/scalper_today/api/routes/__init__.py
@@ -1,8 +1,3 @@
-from .core import router as core_router
-from .events import router as events_router
 from . import auth, alerts
 
-# Backward-compatible alias for previous imports from scalper_today.api.routes import router
-router = events_router
-
-__all__ = ["core_router", "events_router", "router", "auth", "alerts"]
+__all__ = ["auth", "alerts"]
